621b76197415a2b3b7c6d1320f33b448bfda1fad
Kingsley Ihemere <dekings44@gmail.com>
2023-04-17 18:32:43 +0100

    successfully querries all the needed data for my personal weather advisor

--- weather_message.py
+++ weather_message.py
@@ -29,7 +29,30 @@ data_first = response.json()['list'][0]
 data_second = response.json()['list'][1]
 data_third = response.json()['list'][2]
 data_fourth = response.json()['list'][3]
-print(data_fourth)
+
+weather_next_3hrs = data_first['weather'][0]['main']
+weather_desc_next_3hrs = data_first['weather'][0]['description']
+date_time_next_3hrs = data_first['dt_txt']
+
+
+weather_next_6hrs = data_second['weather'][0]['main']
+weather_desc_next_6hrs = data_second['weather'][0]['description']
+date_time_next_6hrs = data_second['dt_txt']
+
+weather_next_9hrs = data_third['weather'][0]['main']
+weather_desc_next_9hrs = data_third['weather'][0]['description']
+date_time_next_9hrs = data_third['dt_txt']
+
+weather_next_12hrs = data_fourth['weather'][0]['main']
+weather_desc_next_12hrs = data_fourth['weather'][0]['description']
+date_time_next_12hrs = data_fourth['dt_txt']
+
+
+
+print(f'The current weather detail: It is {weather_next_3hrs}, and {weather_desc_next_3hrs} during {date_time_next_3hrs}')
+print(f'The current weather detail: It is {weather_next_6hrs}, and {weather_desc_next_6hrs} during {date_time_next_6hrs}')
+print(f'The current weather detail: It is {weather_next_9hrs}, and {weather_desc_next_9hrs} during {date_time_next_9hrs}')
+print(f'The current weather detail: It is {weather_next_12hrs}, and {weather_desc_next_12hrs} during {date_time_next_12hrs}')
 
 
 
